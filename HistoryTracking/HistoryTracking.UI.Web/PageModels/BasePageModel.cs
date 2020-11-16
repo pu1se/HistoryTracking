bc0a574b546f62b72f9025aaefea4f4e49d3c91e
Paul Pontus <pavel.pontus@appxite.com>
2020-11-16 17:30:31 +0300

    added user list displaying

---
+++ HistoryTracking/HistoryTracking.UI.Web/PageModels/BasePageModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HistoryTracking.UI.Web.PageModels
+{
+    public class BasePageModel
+    {
+        public string ErrorMessage { get; set; }
+        public string LogMessage { get; set; }
+        public bool IsActionSuccess { get; set; }
+        public bool IsReady { get; set; }
+    }
+}
---
+++ HistoryTracking/HistoryTracking.UI.Web/PageModels/UserListPageModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HistoryTracking.BL.Services.User;
+
+namespace HistoryTracking.UI.Web.PageModels
+{
+    public class UserListPageModel : BasePageModel
+    {
+        public List<GetUserModel> UserList { get; set; } = new List<GetUserModel>();
+    }
+}
