bea71c43cf90307f0419bffd465f5a02b857cd0d
Paul Pontus <pavel.pontus@appxite.com>
2021-05-12 19:07:24 +0300

    added current user to UI

--- HistoryTracking/HistoryTracking.Api/App_Start/WebApiConfig.cs
+++ HistoryTracking/HistoryTracking.Api/App_Start/WebApiConfig.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Swashbuckle.Application;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http.Headers;
@@ -20,6 +21,13 @@ namespace HistoryTracking.Api
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.Routes.MapHttpRoute(
+                name: "swaggerApiDocumentation",
+                routeTemplate: string.Empty,
+                defaults: null,
+                constraints: null,
+                handler: new RedirectHandler(m => m.RequestUri.ToString(), "swagger"));
 
             config.Formatters.JsonFormatter.SupportedMediaTypes
                 .Add(new MediaTypeHeaderValue("text/html") );
--- HistoryTracking/HistoryTracking.UI.Web/_Core/BaseApiClient.cs
+++ HistoryTracking/HistoryTracking.UI.Web/_Core/BaseApiClient.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace HistoryTracking.UI.Web.ApiRequests
@@ -14,6 +15,12 @@ namespace HistoryTracking.UI.Web.ApiRequests
         {
             Settings = settings;
             Api = new ApiRequestExecuter(Settings.ApiUrl);
+        }
+
+        static BaseApiClient()
+        {
+            ServicePointManager.ServerCertificateValidationCallback +=
+                (sender, certificate, chain, sslPolicyErrors) => true;
         }
     }
 }
