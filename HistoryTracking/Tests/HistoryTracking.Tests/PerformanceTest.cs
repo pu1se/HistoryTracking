a7c6ae5b14b5e025dd5d5827cf9bc7dd72e2d4fe
Paul Pontus <pavel.pontus@appxite.com>
2021-05-31 14:46:53 +0300

    added test method

--- HistoryTracking/Tests/HistoryTracking.Tests/PerformanceTest.cs
+++ HistoryTracking/Tests/HistoryTracking.Tests/PerformanceTest.cs
@@ -1,29 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using HistoryTracking.BL.Services;
 using HistoryTracking.BL.Services.Order.Models;
+using HistoryTracking.BL.Services.SubscriptionProducts;
+using HistoryTracking.BL.Services.SubscriptionProducts.Models;
 using HistoryTracking.DAL;
+using HistoryTracking.DAL.Entities;
 using HistoryTracking.DAL.Enums;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace HistoryTracking.Tests
 {
     [TestClass]
-    public class PerformanceTest : BaseServiceTests<OrderService>
+    public class PerformanceTest : BaseTest
     {
-        /*[TestMethod]
+        [TestMethod]
         public async Task AddEditLoop()
         {
-            await Service.AddEditItem(new AddEditOrderModel
+            var index = 0;
+            while (index++ < 100)
             {
-                CustomerId = TestData.CustomerUserId,
-                OrderDate = DateTime.UtcNow,
-                OrderStatus = OrderStatusType.WaitingForApproval,
-                SubscriptionIdList = new List<Guid> { TestData.SubscriptionProductId }
-            });
-        }*/
+                var id = Guid.NewGuid();
+                var addSubscription = new SubscriptionProductEntity
+                {
+                    Id = id,
+                    Name = "some name" + id,
+                    CreatedByUserId = TestData.ResellerUserId,
+                    UpdatedByUserId = TestData.ResellerUserId,
+                    AccountId = TestData.ResellerUserId,
+                    AutoRenewEnabled = true,
+                    AzureTenantId = TestData.ResellerUserId,
+                    Currency = CurrencyType.Euro,
+                    DistributorMarkupAsPercent = 10,
+                    EntitlementId = Guid.NewGuid().ToString(),
+                };
+
+                Storage.SubscriptionProducts.AddOrUpdate(addSubscription);
+
+                if (index % 5 == 0)
+                {
+                    await Storage.SaveChangesAsync();
+                    CleanStorageCache();
+                }
+                
+            }
+        }
     }
 }
